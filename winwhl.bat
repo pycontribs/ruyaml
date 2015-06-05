61d39624f713ebd9f3bea7d1effed001591b5a11
Anthon van der Neut <anthon@mnt.org>
2015-06-05 09:29:28 +0200

    py3 compatibility wheel for pypy2

--- winwhl.bat
+++ winwhl.bat
@@ -6,3 +6,4 @@ c:\python\3.4\python.exe setup.py bdist_wheel
 c:\python\3.3\python.exe setup.py bdist_wheel
 c:\python\3.4-32\python.exe setup.py bdist_wheel
 c:\python\3.3-32\python.exe setup.py bdist_wheel
+c:\pypy2\2.5\pypy-2.5.1-win32\pypy.exe setup.py bdist_wheel
