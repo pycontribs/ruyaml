a1dab22d7f1f9c9ad6b5834de2c873b8546c667e
Anthon van der Neut <anthon@mnt.org>
2015-06-13 09:57:34 +0200

    added multilevel get on CommentedMap (mlget())

--- py/comments.py
+++ py/comments.py
@@ -259,6 +259,29 @@ class CommentedMap(ordereddict, CommentedBase):
             for x in vals:
                 self[x] = vals[x]
 
+    def mlget(self, key, default=None, list_ok=False):
+        """multi-level get that expects dicts within dicts"""
+        if not isinstance(key, list):
+            return self.get(key, default)
+        # assume that the key is a list of recursively accessible dicts
+        def get_one_level(key_list, level, d):
+            if not list_ok:
+                assert isinstance(d, dict)
+            if level >= len(key_list):
+                if level > len(key_list):
+                    raise IndexError
+                return d[key_list[level-1]]
+            return get_one_level(key_list, level+1, d[key_list[level-1]])
+
+        try:
+            return get_one_level(key, 1, self)
+        except KeyError:
+            return default
+        except (TypeError, IndexError):
+            if not list_ok:
+                Raise
+            return default
+
 
 class CommentedOrderedMap(CommentedMap):
     __slots__ = [Comment.attrib, ]
