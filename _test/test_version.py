5c8d362580ac41318298eb18061272fdb2140c22
Anthon van der Neut <anthon@mnt.org>
2017-08-05 14:58:39 +0200

    no sexagesimal floats in 1.2

--- _test/test_version.py
+++ _test/test_version.py
@@ -62,6 +62,7 @@ class TestVersions:
     def test_implicit_1_2(self):
         l = load("""\
         - 12:34:56
+        - 12:34:56.78
         - 012
         - 012345678
         - 0o12
@@ -72,18 +73,20 @@ class TestVersions:
         - true
         """)
         assert l[0] == '12:34:56'
-        assert l[1] == 12
-        assert l[2] == '012345678'
-        assert l[3] == 10
-        assert l[4] == 'on'
-        assert l[5] == 'off'
-        assert l[6] == 'yes'
-        assert l[7] == 'no'
-        assert l[8] is True
+        assert l[1] == '12:34:56.78'
+        assert l[2] == 12
+        assert l[3] == '012345678'
+        assert l[4] == 10
+        assert l[5] == 'on'
+        assert l[6] == 'off'
+        assert l[7] == 'yes'
+        assert l[8] == 'no'
+        assert l[9] is True
 
     def test_load_version_1_1(self):
         l = load("""\
         - 12:34:56
+        - 12:34:56.78
         - 012
         - 012345678
         - 0o12
@@ -94,11 +97,12 @@ class TestVersions:
         - true
         """, version="1.1")
         assert l[0] == 45296
-        assert l[1] == 10
-        assert l[2] == '012345678'
-        assert l[3] == '0o12'
-        assert l[4] is True
-        assert l[5] is False
-        assert l[6] is True
-        assert l[7] is False
-        assert l[8] is True
+        assert l[1] == 45296.78
+        assert l[2] == 10
+        assert l[3] == '012345678'
+        assert l[4] == '0o12'
+        assert l[5] is True
+        assert l[6] is False
+        assert l[7] is True
+        assert l[8] is False
+        assert l[9] is True
