5087fe3169e6024ed31d8d3cdc2c13d5f5eb33da
Marcel Bargull <marcel.bargull@udo.edu>
2018-03-17 00:00:56 +0100

    add doc string for util.LazyEval

--- util.py
+++ util.py
@@ -17,6 +17,15 @@ if False:  # MYPY
 
 
 class LazyEval(object):
+    """
+    Lightweight wrapper around lazily evaluated func(*args, **kwargs).
+
+    func is only evaluated when any attribute of its return value is accessed.
+    Every attribute access is passed through to the wrapped value.
+    (This only excludes special cases like method-wrappers, e.g., __hash__.)
+    The sole additional attribute is the lazy_self function which holds the
+    return value (or, prior to evaluation, func and arguments), in its closure.
+    """
     def __init__(self, func, *args, **kwargs):
         def lazy_self():
             return_value = func(*args, **kwargs)
