358f690a1b5777b70259bff4d60ef973223f6065
Anthon van der Neut <1210650+AvdN@users.noreply.github.com>
2018-08-12 09:58:37 +0200

    ref issue #160 flow mapping with comments in sequence gobbles newline

--- _test/test_issues.py
+++ _test/test_issues.py
@@ -9,7 +9,7 @@ import pytest  # NOQA
 from roundtrip import round_trip, round_trip_load, round_trip_dump, dedent  # NOQA
 
 
-class TestIssue61:
+class TestIssues:
     def test_issue_61(self):
         import ruamel.yaml
 
@@ -25,3 +25,16 @@ class TestIssue61:
         data = ruamel.yaml.round_trip_load(s)
         assert str(data['comb']) == str(data['def'])
         assert str(data['comb']) == "ordereddict([('key', 'value'), ('key1', 'value1')])"
+
+    def test_issue_160(self):
+        s = dedent("""\
+        root:
+            # a comment
+            - {some_key: "value"}
+        
+        foo: 32
+        bar: 32
+        """)
+        x = round_trip(s, block_seq_indent=4, preserve_quotes=True)
+        assert x['bar'] == 32
+        
