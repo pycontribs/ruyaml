112b846a58ab42c795788fcbbc12ff92e50932cb
Anthon van der Neut <1210650+AvdN@users.noreply.github.com>
2018-09-01 09:27:55 +0200

    caching indirected method call for minor speed improvements on reading, re #159

--- reader.py
+++ reader.py
@@ -143,7 +143,7 @@ class Reader(object):
             self.update(length)
         return self.buffer[self.pointer : self.pointer + length]
 
-    def forward(self, length=1):
+    def forward_1_1(self, length=1):
         # type: (int) -> None
         if self.pointer + length + 1 >= len(self.buffer):
             self.update(length + 1)
@@ -152,6 +152,23 @@ class Reader(object):
             self.pointer += 1
             self.index += 1
             if ch in u'\n\x85\u2028\u2029' or (
+                ch == u'\r' and self.buffer[self.pointer] != u'\n'
+            ):
+                self.line += 1
+                self.column = 0
+            elif ch != u'\uFEFF':
+                self.column += 1
+            length -= 1
+
+    def forward(self, length=1):
+        # type: (int) -> None
+        if self.pointer + length + 1 >= len(self.buffer):
+            self.update(length + 1)
+        while length != 0:
+            ch = self.buffer[self.pointer]
+            self.pointer += 1
+            self.index += 1
+            if ch == u'\n' or (
                 ch == u'\r' and self.buffer[self.pointer] != u'\n'
             ):
                 self.line += 1
